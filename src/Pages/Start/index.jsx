a4b94f08319ceec51f421fbc61ddcb1a8587cff7
rbcunha <barrosdacunha@gmail.com>
2022-11-30 11:41:45 -0300

    atualização arquivos aula01

---
+++ src/Pages/Start/index.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { View, Text, Image, ScrollView, StyleSheet } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+
+import DefaultButton from '../../Components/Common/DefaultButton'
+import LifeStatus from '../../Components/Common/LifeStatus'
+
+export default function Start() {
+  const handleNavAppExplanation = () => {
+    console.log('Testando o clique!')
+  }
+
+  return (
+    <View style={styles.container}>
+      <ScrollView style={styles.scroll} showsVerticalScrollIndicator={false}>
+        <View style={{ alignItems: 'center' }}>
+          <Image
+            source={require('../../assets/icons/logo3.png')}
+            style={styles.logo}
+          />
+          <LifeStatus />
+        </View>
+        <Text style={styles.description}>
+          Vamos transformar sua vida {'\n'} em um jogo, buscando sempre {'\n'}
+          {''} o melhor nivel.
+        </Text>
+        <DefaultButton
+          buttonText={'Continuar'}
+          handlePress={handleNavAppExplanation}
+          width={250}
+          height={50}
+        />
+      </ScrollView>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'rgba(21, 21, 21, 0.98)'
+  },
+  logo: {
+    width: 300,
+    height: 60,
+    marginTop: 60,
+    marginBottom: 20
+  },
+
+  description: {
+    color: '#FFFFFF',
+    fontSize: 20,
+    textAlign: 'center',
+    marginVertical: 60
+  }
+})
---
+++ src/Routes/AllPages.jsx
@@ -0,0 +1,22 @@
+import { NavigationContainer, StackActions } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import Start from '../Pages/Start'
+import AppExplanation from '../Pages/AppExplanation'
+
+const Stack = createNativeStackNavigator()
+
+export default function AllPages() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+      >
+        <Stack.Screen name="AppExplanation" component={AppExplanation} />
+        <Stack.Screen name="Start" component={Start} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
+}
